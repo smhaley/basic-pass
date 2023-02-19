5f9df4be306a41766ed4e145afcc640a4c78d355
shawn <shawnh87@gmail.com>
2023-02-19 12:58:27 -0500

    split out import flow

--- src/components/manager/sidenav/import/Import.svelte
+++ src/components/manager/sidenav/import/Import.svelte
@@ -1,22 +1,14 @@
 <script lang="ts">
-  import { userStore, logout } from "../../../../stores/store";
-  import { BasicCrypto } from "../../../../utils/crypto/encrypt";
-  import * as ErrorUtils from "../../../error-utils";
-  import InputSection from "../../../../lib/InputSection.svelte";
-  import type { TableData, TableEntry } from "../../../../stores/tableStore";
-  import { deepCopyTable } from "../../../../stores/utils";
+  import ImportConfirm from "./ImportConfirm.svelte";
+  import { fade } from "svelte/transition";
 
   let cypherText: string;
   let filename: string;
-  let storePass: string;
-  let decryptedTableData: TableData;
-  let cypherData: string;
-  let previewData: string;
-
-  let passphraseError: ErrorUtils.ErrObj = ErrorUtils.baseError;
+  let mountKey = 0;
 
   const handleUpload = () => {
-    handleImportAbort();
+    mountKey = mountKey++;
+    resetImportState();
     const file = (
       document.querySelector("input[type=file]") as HTMLInputElement
     ).files[0];
@@ -36,44 +28,14 @@
     };
   };
 
-  const handleStoreDecrypt = (e: Event) => {
-    e.preventDefault();
-    passphraseError = ErrorUtils.baseError;
-    const validTextErr = ErrorUtils.validateText(storePass);
-    if (!validTextErr.missing) {
-      const basicCrypto = new BasicCrypto(storePass, $userStore.username);
-      try {
-        const tableData = basicCrypto.decryptTable(cypherText);
-        decryptedTableData = tableData;
-        cypherData = cypherText;
-        const tableCopy = deepCopyTable(tableData);
-        Object.values(tableCopy).forEach(
-          (entry: TableEntry) => delete entry.passphrase
-        );
-        previewData = JSON.stringify(tableCopy, null, 2);
-      } catch (e) {
-        passphraseError = { ...ErrorUtils.baseError, invalid: true };
-      }
-    } else {
-      passphraseError = validTextErr;
-    }
-  };
-
-  const handleImportAbort = () => {
+  const resetImportState = () => {
+    mountKey = mountKey++;
     cypherText = undefined;
     filename = undefined;
-    storePass = undefined;
-    decryptedTableData = undefined;
-    passphraseError = ErrorUtils.baseError;
   };
 
-  const handleNewDataStore = async () => {
-    await BasicCrypto.handleSrcTableUpdate(
-      decryptedTableData,
-      storePass,
-      $userStore.username
-    );
-    logout();
+  const handleCancel = (e: Event) => {
+    resetImportState();
   };
 </script>
 
@@ -82,75 +44,32 @@
     <h2>Table Import</h2>
   </div>
   {#if !filename}
-    <p>You may import a previously exported table store.</p>
-    <strong
-      >Once a table import is finalized, the change cannot be undone.</strong
-    >
-    <p>
-      All aspects of the the import, including the password, will overwrite
-      those of the current logged in table account.
-    </p>
+    <div in:fade={{ delay: 90 }}>
+      <p>You may import a previously exported table store.</p>
+      <strong
+        >Once a table import is finalized, the change cannot be undone.</strong
+      >
+      <p>
+        All aspects of the the import, including the password, will overwrite
+        those of the current logged in table account.
+      </p>
 
-    <p>
-      The password used to decrypt the imported table will become the password
-      for the current logged in user.
-    </p>
-
-    <div class="button-group">
-      <label for="file-upload" class="file-upload primary-button">
-        Store Upload
-      </label>
-      <input id="file-upload" type="file" on:change={handleUpload} />
-    </div>
-  {:else if !decryptedTableData}
-    <p class="current-file">
-      Current file: {filename}
-    </p>
-    <form on:submit={handleStoreDecrypt}>
-      <InputSection
-        type="password"
-        label={"Import Store Password"}
-        errs={passphraseError}
-        errMsgs={ErrorUtils.loginPassErrMsgs}
-        bind:value={storePass}
-      />
+      <p>
+        The password used to decrypt the imported table will become the password
+        for the current logged in user.
+      </p>
 
       <div class="button-group">
-        <div class="button-child">
-          <button class="secondary-button" on:click={handleImportAbort}
-            >Cancel</button
-          >
-          <button class="primary-button">Submit</button>
-        </div>
+        <label for="file-upload" class="file-upload primary-button">
+          Store Upload
+        </label>
+        <input id="file-upload" type="file" on:change={handleUpload} />
       </div>
-    </form>
+    </div>
   {:else}
-    <p>Confirm User Store and Passphrase Override</p>
-    <p>Please verify the preview data below.</p>
-    <p>
-      On confirm, your current logged in store will be overwritten with the
-      store below.
-    </p>
-    <p>
-      Additionally the access passphrase will will become the passphrase used to
-      unlock the data below.
-    </p>
-    <p>
-      On confirm you will be logged out. On login you will see your uploaded
-      table data.
-    </p>
-    <p>Table preview with scrubbed passphrases:</p>
-    <div class="preview">
-      <pre>{previewData}</pre>
-    </div>
-    <div class="button-group ">
-      <button class="danger-button overwrite" on:click={handleNewDataStore}>
-        Confirm Overwrite</button
-      >
-      <button class="secondary-button overwrite" on:click={handleImportAbort}>
-        Cancel</button
-      >
-    </div>
+    {#key mountKey}
+      <ImportConfirm on:cancel={handleCancel} {filename} {cypherText} />
+    {/key}
   {/if}
 </div>
 
@@ -172,18 +91,5 @@
     margin-top: 16px;
     max-width: 260px;
     margin-left: auto;
-  }
-  .button-child {
-    margin-left: auto;
-  }
-  .current-file {
-    font-size: 1.25rem;
-  }
-  .preview {
-    max-height: 600px;
-    overflow-y: scroll;
-  }
-  .overwrite {
-    max-width: 400px;
   }
 </style>
